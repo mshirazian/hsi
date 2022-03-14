0a06e5393a2ee6af6489975426015c4c96c8925e
mshirazian <mshirazian@gmail.com>
2022-03-13 22:06:34 -0700

    version 2

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,10 @@
 import leafmap.foliumap as leafmap
 import folium
+import folium
 import streamlit as st
+import webbrowser
+
+url = 'https://soterai.z22.web.core.windows.net/App/index.html'
 import streamlit.components.v1 as components
 import streamlit_authenticator as stauth
 # import ee
@@ -27,7 +31,7 @@ passwords = ['123','456']
 tiles = None
 width = 800
 height = 600
-
+access_token = 'pk.eyJ1IjoibXNoaXJhemlhbiIsImEiOiJjbDBxNnY4ZnoxcjNzM2NvMHpjaXgwZ3l6In0.a465HCh_wNlTEzwc4jDFdw'
 # hashed_passwords = stauth.hasher(passwords).generate()
 
 HtmlFile = open("assets/sketchfab.html", 'r', encoding='utf-8')
@@ -42,30 +46,38 @@ source_code = HtmlFile.read()
     # html = "assets\sfo.html"
     # leafmap.cesium_to_streamlit(html, height=800)
     # m = geemap.Map(location=[33.81712, -118.34365], zoom_start=18)
-m = leafmap.Map(location=[33.81712, -118.34365], zoom_start=18)
-folium.Marker([33.81712, -118.34365], popup = ' HSI Lab ').add_to(m)
+m = leafmap.Map(location=[33.81712, -118.34365], zoom_start=17)
+# folium.Marker([33.81712, -118.34365], popup =' HSI Lab ').add_to(m)
 
-folium.TileLayer('Stamen Terrain').add_to(m)
-folium.TileLayer('Stamen Toner').add_to(m)
-folium.TileLayer('Stamen Water Color').add_to(m)
-folium.TileLayer('cartodbpositron').add_to(m)
-folium.TileLayer('cartodbdark_matter').add_to(m)
-folium.LayerControl().add_to(m)
+
+# folium.TileLayer('mapbox').add_to(m)
+#folium.TileLayer('Stamen Toner').add_to(m)
+# folium.TileLayer('Stamen Water Color').add_to(m)
+# folium.TileLayer('cartodbpositron').add_to(m)
+# folium.TileLayer('cartodbdark_matter').add_to(m)
+# folium.TileLayer('MapQuest Open Aerial').add_to(m)
+# folium.LayerControl().add_to(m)
 # CircleMarker with radius
-folium.CircleMarker(location = [33.81730, -118.343395], radius = 50, fill_color='blue',popup = ' HSI Lab ').add_to(m)
 
+pp = folium.Html('<a href="'+ url+'"target="_blank">'+ 'Pipes Digital Twin' + '</a>', script=True)
+popup = folium.Popup(pp, max_width=2650)
+folium.CircleMarker(location=[33.81730, -118.343395], radius=20, fill_color='blue', popup=popup).add_to(m)
+# folium.Marker([30,-100], popup=popup).add_to(m)
+
+st.image("assets/logo_gold.png", width=100)
+# if st.button('Open browser'):
+#     webbrowser.open(url)
 if tiles is not None:
     for tile in tiles:
         m.add_xyz_service(tile)
 
-# m.to_streamlit(width, height)
-with st.sidebar:
+m.to_streamlit(width, height)
+# with st.sidebar:
     # st.write('Welcome *%s*' % (st.session_state['name']))
-    st.image("assets/logo_gold.png", width=200)
-    selection = st.selectbox('Facility:', ['...','HSI Lab'])
-if selection ==  'HSI Lab':
-        components.html(source_code)
-        m.to_streamlit(width, height)
+# selection = st.selectbox('Facility:', ['...','HSI Lab'])
+# if selection ==  'HSI Lab':
+#         components.html(source_code)
+#         m.to_streamlit(width, height)
 # elif st.session_state['authentication_status'] == False:
 #     st.error('Username/password is incorrect')
 # elif st.session_state['authentication_status'] == None:
